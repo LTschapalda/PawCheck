732ffeaefec68b12661be74f795146d6b520cce5
leonietschapalda <ltschapalda@googlemail.com>
2023-10-18 12:53:29 +0200

    flapdoodle

---
+++ backend/src/test/java/de/pawcheck/backend/BackendApplicationTest.java
@@ -0,0 +1,13 @@
+package de.pawcheck.backend;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BackendApplicationTest {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+}
