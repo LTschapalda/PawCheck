e8bbb33174bd687ae37dd47a9b8b8b4f6c0b2b41
leonietschapalda <ltschapalda@googlemail.com>
2023-10-18 16:37:39 +0200

    Landing Page

---
+++ frontend/src/Components/LandingPage.tsx
@@ -0,0 +1,11 @@
+export default function LandingPage() {
+    return (
+        <>
+            <div className="topic">
+                <h1>Bereit für deinen Trip?</h1>
+                <p>Let's check, ob deine Katze es auch ist!</p>
+            </div>
+            <img id="pawcheck" src="src/assets/PawCheck.svg" alt="PawCheck"/>
+        </>
+    )
+}
