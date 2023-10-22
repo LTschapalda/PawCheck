0058a92bcf0bc89376f80587458d21935e9a2602
leonietschapalda <ltschapalda@googlemail.com>
2023-10-22 15:59:42 +0200

    User raus, angefangen auf Cat umzubauen, funktioniert aber noch nicht...

--- frontend/src/Components/NameInput.tsx
+++ frontend/src/Components/NameInput.tsx
@@ -3,15 +3,15 @@ import {ChangeEvent, useState} from "react";
 import axios from "axios";
 
 export default function NameInput() {
-    const [value, setValue] = useState<string>('')
+    const [name, setName] = useState<string>('')
 
     function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
-        const value: string = event.target.value;
-        setValue(value);
+        const name: string = event.target.value;
+        setName(name);
     }
 
     const handleSubmit = () => {
-        axios.post('cat/name', { value })
+        axios.post('api/cat', `"${name}"`, {headers: {'Content-Type': 'text/plain',},})
             .then(response => {
                 console.log(response.data);
             })
@@ -27,12 +27,12 @@ export default function NameInput() {
             </div>
             <div className="nameinput">
                 <svg width="305" height="97" viewBox="0 0 305 97" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M26 95L38 69H16.5C7 69 1 61 1 52V19C1 10.5 7.5 1.5 16.5 1.5H287C296 1.5 304 7.5 304 19V52C304 62 300.5 69 287 69H75C64.6 82.6 38 92 26 95Z" fill="white" stroke="#87AD19" stroke-width="2"/>
+                    <path d="M26 95L38 69H16.5C7 69 1 61 1 52V19C1 10.5 7.5 1.5 16.5 1.5H287C296 1.5 304 7.5 304 19V52C304 62 300.5 69 287 69H75C64.6 82.6 38 92 26 95Z" fill="white" stroke="#87AD19" strokeWidth="2"/>
                     <foreignObject  x="10" y="10" width="285" height="50">
                         <input className="speech"
                                type="text"
                                placeholder="Hier eingeben"
-                               value={value}
+                               value={name}
                                onChange={handleInputChange}/>
                     </foreignObject>
                 </svg>
