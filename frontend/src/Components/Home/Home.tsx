fd50bc106e49a8da3d41ff9605973fda4e46b0e0
leonietschapalda <ltschapalda@googlemail.com>
2023-10-24 16:36:47 +0200

    Add Cat Button in the Home Menu

--- frontend/src/Components/Home/Home.tsx
+++ frontend/src/Components/Home/Home.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import {useEffect, useState} from "react";
 import {Cat} from "../assets/Cat.ts";
 import CatHeader from './CatHeader.tsx';
+import {Link} from "react-router-dom";
 
 export default function Home() {
 
@@ -20,10 +21,23 @@ export default function Home() {
 
     return(
         <>
-            <div className="xx"></div>
+            <div className="xx">
+                <div className="topic">
+                <h1>Hallo Nutzer</h1>
+                    <p>Hier findest du eine Übersicht über deine Katzen</p>
+                </div>
+            </div>
             {catsOwned.map(
                 (cat:Cat) => <CatHeader key={cat.id} cat={cat}/>
             )}
+
+            <div className="addCat">
+                <Link to="/cat/name">
+                <svg id="plus" width="27" height="27" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13.8966 2V25M25 13.3708H2" stroke="white" stroke-width="3" stroke-linecap="round"/>
+                </svg>
+                </Link>
+            </div>
 
         </>
     )
