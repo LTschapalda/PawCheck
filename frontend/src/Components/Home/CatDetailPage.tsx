39585ab8888451520860d1a34888e71eccf76133
leonietschapalda <ltschapalda@googlemail.com>
2023-10-27 12:23:46 +0200

    Delete-confirmation-Popup :)

--- frontend/src/Components/Home/CatDetailPage.tsx
+++ frontend/src/Components/Home/CatDetailPage.tsx
@@ -2,6 +2,7 @@ import CatFace from "../../assets/ImagePlaceholder.png";
 import {useNavigate, useParams} from "react-router-dom";
 import {Cat} from "../assets/Cat.ts";
 import axios from "axios";
+import {useState} from "react";
 
 type CatDetailProps = {
     catsOwned : Cat[]
@@ -12,6 +13,17 @@ export default function CatDetailPage( props: CatDetailProps) {
     const { id } = useParams();
     const navigate = useNavigate()
     const selectedCat = props.catsOwned.find((cat: Cat) => cat.id == id)
+
+    const [deleteConfirmation, setDeleteConfirmation] = useState(false)
+    const toggleDeleteConfirmation = () => {
+        setDeleteConfirmation(!deleteConfirmation);
+    }
+
+    if(deleteConfirmation) {
+        document.body.classList.add('active-deleteConfirmation')
+    } else {
+        document.body.classList.remove('active-deleteConfirmation')
+    }
 
     //FUNCTIONS
     function deleteCatEverywhere () {
@@ -39,7 +51,21 @@ export default function CatDetailPage( props: CatDetailProps) {
                 )}
             </div>
             <div id="deleteCat" className="deleteCat"
-                 onClick={deleteCatEverywhere}>Löschen</div>
+                 onClick={toggleDeleteConfirmation}>Löschen
+            </div>
+
+            {deleteConfirmation && (
+                <div className="deleteConfirmationPopup">
+                    <div onClick={toggleDeleteConfirmation} className="overlay"></div>
+                    <div className="deleteConfirmationPopup-content">
+                        <h3>Willst du diese Katze wirklich endgültig löschen?</h3>
+                        <button className="mainButton"
+                                onClick={toggleDeleteConfirmation}>Abort!</button>
+                        <button className="secondaryButton"
+                                onClick={deleteCatEverywhere}>Ja, ich bin sicher</button>
+                    </div>
+                </div>
+            )}
         </>
     )
 }
