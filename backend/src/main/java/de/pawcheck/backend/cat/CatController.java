f4e174fc6026e584c2b2f1d7ca81c2c984f25014
leonietschapalda <ltschapalda@googlemail.com>
2023-11-02 14:56:54 +0100

    Backend: Tests

--- backend/src/main/java/de/pawcheck/backend/cat/CatController.java
+++ backend/src/main/java/de/pawcheck/backend/cat/CatController.java
@@ -2,7 +2,6 @@ package de.pawcheck.backend.cat;
 
 import de.pawcheck.backend.user.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- backend/src/test/java/de/pawcheck/backend/user/UserServiceTest.java
+++ backend/src/test/java/de/pawcheck/backend/user/UserServiceTest.java
@@ -4,6 +4,7 @@ import de.pawcheck.backend.IdService;
 import de.pawcheck.backend.cat.Cat;
 import de.pawcheck.backend.cat.CatRepo;
 import de.pawcheck.backend.cat.CatService;
+import de.pawcheck.backend.cat.Food;
 import org.junit.jupiter.api.Test;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
@@ -23,14 +24,14 @@ class UserServiceTest {
     UserService userService = new UserService(userRepo, catService);
 
     @Test
-    void GetAListOFCatsWhen_getCatsAssociatedToUser() {
+    void getAListOFCatsWhen_getCatsAssociatedToUser() {
         //GIVEN
         Cat newCat = new Cat("1234", "Mo");
         User user = new User("123", List.of("1234"));
 
         // Mocks und Annahmen
         when(userRepo.findById(user.id())).thenReturn(Optional.of(user));
-        when(catRepo.findById(newCat.id())).thenReturn(Optional.of(newCat));
+        when(catRepo.findById(newCat.getId())).thenReturn(Optional.of(newCat));
 
         // WHEN
         List<Cat> expected = userService.getCatsAssociatedToUser(user.id());
@@ -41,14 +42,14 @@ class UserServiceTest {
     }
 
     @Test
-    void GetEmptyListWhen_getCatsAssociatedToUser_UserIsNotPresent() {
+    void getEmptyListWhen_getCatsAssociatedToUser_UserIsNotPresent() {
         //GIVEN
         Cat newCat = new Cat("1234", "Mo");
         User user = new User("123", List.of("1234"));
 
         // Mocks und Annahmen
         when(userRepo.findById(user.id())).thenReturn(Optional.empty());
-        when(catRepo.findById(newCat.id())).thenReturn(Optional.of(newCat));
+        when(catRepo.findById(newCat.getId())).thenReturn(Optional.of(newCat));
 
         // WHEN
         List<Cat> expected = userService.getCatsAssociatedToUser(user.id());
@@ -56,5 +57,40 @@ class UserServiceTest {
         // THEN
         verify(userRepo, times(1)).findById(user.id());
         assertEquals(expected, List.of());
+    }
+
+    @Test
+    void getUpdatedCat_WhenUpdateCatById() {
+        //GIVEN
+        String catId = "1234";
+        Cat existingCat = new Cat("1234", "Mo", new Food("40g", "30g"), new Food("50g", "40g"));
+        Cat updatedCat = new Cat("1234", "NewName", new Food("20g", "10g"), new Food("30g", "20g"));
+
+        when(catRepo.findById(catId)).thenReturn(Optional.of(existingCat));
+        when(catRepo.save(any())).thenReturn(updatedCat);
+
+        //WHEN
+        Cat result = catService.updateCatById(catId, updatedCat);
+        //THEN
+        verify(catRepo, times(1)).findById(catId);
+        verify(catRepo, times(1)).save(existingCat);
+        assertEquals(updatedCat, result);
+    }
+
+    @Test
+    void updateCatById_nonExistingCat_shouldReturnNull() {
+        //GIVEN
+        String catId = "nonExistentId";
+        Cat updatedCat = new Cat("nonExistentId", "NewName", new Food("20g", "10g"), new Food("30g", "20g"));
+
+        when(catRepo.findById(catId)).thenReturn(Optional.empty());
+
+        //WHEN
+        Cat result = catService.updateCatById(catId, updatedCat);
+
+        //THEN
+        verify(catRepo, times(1)).findById(catId);
+        verify(catRepo, never()).save(any());
+        assertEquals(null, result);
     }
 }
