966d55b3dddd16ef3456a58338caee6c88f1d89f
leonietschapalda <ltschapalda@googlemail.com>
2023-11-08 17:38:58 +0100

    Backend: User wird in der Datenbank gesucht und angelegt, falls noch nicht vorhanden

--- backend/src/main/java/de/pawcheck/backend/user/UserController.java
+++ backend/src/main/java/de/pawcheck/backend/user/UserController.java
@@ -1,18 +1,13 @@
 package de.pawcheck.backend.user;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-
-
     UserService userService;
 
     //Konstruktor
@@ -24,7 +19,7 @@ public class UserController {
     public User getUser() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (auth instanceof OAuth2AuthenticationToken token) {
-            return userService.showUser(token);
+            return userService.handleLogin(token);
         }
         return null;
     }
